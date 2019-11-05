e12e2ff99f46a1a301002eb9df5870ac74a91ad3
Kassandra Desgagné <kassandra.desgagne@polymtl.ca>
2019-11-05 14:35:08 -0500

    Fichier en date du 5 novembre 2019

---
+++ foncteur.h
@@ -0,0 +1,61 @@
+/********************************************
+* Titre: Travail pratique #5 - foncteur.h
+* Date: 30 octobre 2019
+* Auteur: Allan BEDDOUK & Jeffrey LAVALLEE
+*******************************************/
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+
+#include "coupon.h"
+#include "membrePremium.h"
+
+class AjouterCoupon
+{
+public: 
+	AjouterCoupon(vector<Coupon*>& conteneur) { copy(conteneur.begin(), conteneur.end(), conteneur_.begin()); }
+	vector<Coupon*> operator()(Coupon* coupon) {
+		if (find(conteneur_.begin(), conteneur_.end(), coupon) == conteneur_.end()) {
+			conteneur_.push_back(coupon);
+		}
+		return conteneur_;
+	}
+
+private: 
+	vector<Coupon*> conteneur_;
+};
+
+
+class AjouterMembre
+{
+public:
+	AjouterMembre(map<string, Membre*>& conteneur) { conteneur_ = conteneur;}
+	map<string, Membre*> operator()(pair<string, Membre*> membre) {
+		if (find(conteneur_.begin(), conteneur_.end(), membre) == conteneur_.end()) {
+			conteneur_.insert(membre);
+		}
+		return conteneur_;
+	}
+
+private:
+	map<string, Membre*> conteneur_; 
+};
+
+class IntervallePrixBillet
+{
+public:
+	IntervallePrixBillet(double borneInf, double borneSup) { borneInf_ = borneInf; borneSup_ = borneSup; }
+	bool operator()(Billet* billet) {
+		if (billet->getPrix() >= borneInf_ && billet->getPrix() <= borneSup_) {
+			return true;
+		}
+		else { return false; }
+	}
+
+private:
+	double borneInf_;
+	double borneSup_;
+};
---
+++ gestionnaireMembres.cpp
@@ -0,0 +1,86 @@
+/********************************************
+* Titre: Travail pratique #5 - gestionnaireMembres.cpp
+* Date: 30 octobre 2019
+* Auteur: Allan BEDDOUK & Jeffrey LAVALLEE
+*******************************************/
+
+#include "GestionnaireMembres.h"
+#include <numeric>
+
+void GestionnaireMembres::assignerBillet(Billet* billet, const string& nomMembre, int rabaisCoupon)
+{
+	Membre* membre = conteneur_[nomMembre];
+
+	if (membre == nullptr) {
+		delete billet;
+		return;
+	}
+
+	double prix = billet->getPrix();
+
+	if (auto solde = dynamic_cast<Solde*>(billet)) {
+		prix = solde->getPrixBase();
+	}
+
+	
+	prix -= rabaisCoupon;
+	
+
+	if (auto membrePremium = dynamic_cast<MembrePremium*>(membre)) {
+		double rabais = 0.005 * membrePremium->getpointsCumulee() / 1000;
+		if (rabais > 0.1)
+			rabais = 0.1;
+
+		prix *= (1 - rabais);
+	}
+
+	billet->setPrix(prix);
+	membre->ajouterBillet(billet);
+}
+
+
+
+double GestionnaireMembres::calculerRevenu() const
+{
+	double revenu = 0; 
+	for_each(conteneur_.begin(), conteneur_.end(), [&revenu](pair<string, Membre*> m) {
+		for_each(m.second->getBillets().begin(), m.second->getBillets().end(), [&revenu](Billet* billet) {
+			revenu += billet->getPrix(); });
+		});
+
+	//TODO
+	/*double revenu = 0;
+	for (size_t i = 0; i < membres_.size(); ++i) {
+		for (size_t j = 0; j < membres_[i]->getBillets().size(); ++j) {
+			revenu += membres_[i]->getBillets()[j]->getPrix();
+		}
+	}
+
+	return revenu;
+	8*/
+}
+
+int GestionnaireMembres::calculerNombreBilletsEnSolde() const
+{
+	//TODO
+	int nbBilletsSolde = 0;
+	for (size_t i = 0; i < membres_.size(); ++i) {
+		for (size_t j = 0; j < membres_[i]->getBillets().size(); ++j) {
+			if (dynamic_cast<Solde*>(membres_[i]->getBillets()[j])) {
+				++nbBilletsSolde;
+			}
+		}
+	}
+
+	return nbBilletsSolde;
+}
+
+void GestionnaireMembres::afficher(ostream& o) const
+{
+	//TODO
+	o << "=================== ETAT ACTUEL DU PROGRAMME ==================\n\n";
+
+	for (size_t i =0 ; i<membres_.size() ; ++i) {
+		membres[i]->afficher(o);
+	}
+}
